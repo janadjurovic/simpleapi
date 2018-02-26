463e58b1d474f6d981c2f29b9b6daa41e909e4da
janadjurovic <djurovic.jana@outlook.com>
2018-02-26 02:16:38 +0100

    obrada granicnih slucajeva

--- file.py
+++ file.py
@@ -8,30 +8,45 @@ def as_json(object):
 
 class Employee:
 
-    def __init__(self, params):
-        self.id = params["id"]
-        self.name = params["name"]
-        self.position = params["position"]
-        self.department_id = params["department_id"]
- 
+    def __init__(self, params): 
+        self.id = params['id']
+        self.name = params['name']
+        self.position = params['position']
+        self.department_id = params['department_id']    
+    
 class Department:
     
     def __init__(self, params):
-        self.id = params["id"]
-        self.name = params["name"]
+        self.id = params['id']
+        self.name = params['name']
 
 def get_employee_params(request):
+    if 'content-type' in request.headers and request.headers['content-type'] == 'application/json':
+        return {
+            'id': request.json['id'],
+            'name': request.json['name'],
+            'position': request.json['position'],
+            'department_id': request.json['department_id']
+        }
+        
     return {
-        "id" : request.json["id"],
-        "name" : request.json["name"],
-        "position" : request.json["position"],
-        "department_id" : request.json["department_id"]
-      }
+        'id': request.args.get('id').encode('utf-8'),
+        'name': request.args.get('name').encode('utf-8'),
+        'position': request.args.get('position').encode('utf-8'),
+        'department_id': request.args.get('department_id').encode('utf-8')
+    }
+
+
 
 def get_department_params(request):
+    if 'content-type' in request.headers and request.headers['content-type'] == 'application/json':
+        return {
+            'id' : request.json['id'],
+            'name' : request.json['name']
+        }
     return {
-        "id" : request.json["id"],
-        "name" : request.json["name"]
+        'id': request.args.get('id').encode('utf-8'),
+        'name': request.args.get('name').encode('utf-8')
     }
 
 def authentificate(request):
@@ -42,6 +57,22 @@ def authentificate(request):
 
 employees=[]
 departments=[]
+
+def get_department_ids():
+    department_ids = []
+    for dep in departments:
+        department_ids.append(dep.id)
+    return department_ids    
+
+def check_employee_params(params):
+    if params["name"] == "":
+        raise RuntimeError('Invalid arguments')
+    if not params["department_id"] in get_department_ids():
+        raise RuntimeError('Invalid arguments')
+
+def check_department_params(params):
+    if params["name"] == "":
+        raise RuntimeError('Invalid arguments')
 
 @app.route('/employee', methods=['GET'])
 def get_employees():
@@ -55,17 +86,27 @@ def get_departments():
 
 @app.route('/employee', methods=['POST'])
 def create_employee():
-    authentificate(request)
-    employee = Employee(get_employee_params(request))
-    employees.append(employee)
-    return json.dumps(employee, default = as_json)
+    try:
+        authentificate(request)
+        params = get_employee_params(request)
+        check_employee_params(params)
+        employee = Employee(params)
+        employees.append(employee)
+        return json.dumps(employee, default = as_json)
+    except:
+        abort(400)
 
 @app.route('/department', methods=['POST'])
 def create_department():
-    authentificate(request)
-    department = Department(get_department_params(request))
-    departments.append(department)
-    return json.dumps(department, default = as_json)
+    try:
+        authentificate(request)
+        params = get_department_params(request)
+        check_department_params(params)
+        department = Department(params)
+        departments.append(department)
+        return json.dumps(department, default = as_json)
+    except:
+        abort(400)
         
 @app.route('/', defaults={'path': ''}, methods = ['GET', 'POST'])
 @app.route('/<path:path>',methods = ['GET', 'POST'])
@@ -73,5 +114,5 @@ def catch_all(path):
     abort(403)
 
 if __name__ == '__main__':
-    app.run(debug = True, port = 5000)
+    app.run(host = '0.0.0.0', port = 5001)
 
